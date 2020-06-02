6a5c0e075b79c8edd4385ae881ae998b558399bf
Hari Navaneeth S K <54598618+HariNavaneeth@users.noreply.github.com>
2020-06-02 23:25:11 +0530

    Add files via upload

---
+++ House-Price-Prediction.py
@@ -0,0 +1,35 @@
+#Importing Required Library
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+#Creation of Model begins here
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+#Getting Required Inputs
+number=int(input("Enter Number of Sample Houses : "))
+while(number<5):
+    number=int(input("Enter atleast 5 Sample Houses : "))
+no_of_rooms=[]
+house_price=[]
+print("==============================================================================")
+for i in range(1,number+1):
+    print("Enter House",i,":")
+    no_of_rooms.append(float(input(" Number of Rooms : ")))
+    house_price.append(float(input(" House Price     : ")))
+    print("==============================================================================")
+
+#Model Training
+print("Wait for Few Seconds to Train a Model......")
+xs = np.array(no_of_rooms, dtype=float)
+ys = np.array(house_price, dtype=float)
+model.fit(xs, ys, epochs=1000)
+print("==============================================================================")
+
+#Prediction takes place here
+predict_input=float(input("Enter number rooms required in your house : "))
+print(*model.predict([predict_input]))
+print("==============================================================================")
+
+#Done Happy Coding
